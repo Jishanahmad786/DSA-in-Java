70a172621fb003e5c25dc31f23862b538e29691b
Md Jishan Ahmad <mdjishanahmad442@gmail.com>
2025-01-10 00:44:32 +0530

    file added

--- Array/Last_Occurance.java
+++ Array/Last_Occurance.java
@@ -1,23 +1,22 @@
+// problem -> write a program to last occurance index in array
+
 public class Last_Occurance {
 
-    public static void Solve(int arr[],int x)
-    {
-        int lastIndex=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]==x)
-            {
-                lastIndex=i;
+    public static void Solve(int arr[], int x) {
+        int lastIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                lastIndex = i;
             }
-           
 
         }
         System.out.println(lastIndex);
     }
+
     public static void main(String[] args) {
-        int arr[]={1,2,3,2,4,5,4,7,8,9,8};
-        int x=8;
-        Solve(arr,x);
+        int arr[] = { 1, 2, 3, 2, 4, 5, 4, 7, 8, 9, 8 };
+        int x = 8;
+        Solve(arr, x);
     }
-    
+
 }
